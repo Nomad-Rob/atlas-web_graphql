3a3b3d491c196ee9128e7f80bee12a02a4de9e1f
Nomad-Rob <rfarley89@gmail.com>
2024-03-01 15:56:29 -0600

    Task 1 complete. Set up Root Query and impported it using object destructuring syntax.

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,16 +1,21 @@
 // Initialize the server set on port 8080
 
+// Importing the express module
 const express = require('express');
+// Importing the express-graphql middleware
 const {graphqlHTTP} = require('express-graphql');
+// Importing the TaskType from the schema
 const { TaskType } = require('./schema/schema.js');
 
 const app = express();
 
+// Setting up the graphql middleware
 app.use('/graphql',graphqlHTTP({
   schema: TaskType,
   graphiql: true,
 }));
 
+// Listening for requests on port 8080
 app.listen(8080,()=>{
   console.log('now listening for request on port 8080');
 });
--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -2,6 +2,7 @@
 
 const { GraphQLSchema, GraphQLObjectType, GraphQLInt, GraphQLString } = require('graphql');
 
+// Define the TaskType
 const TaskType = new GraphQLObjectType({
   name: 'Task',
   description: 'This represents a Task',
@@ -13,4 +14,21 @@ const TaskType = new GraphQLObjectType({
   })
 });
 
-module.exports = { TaskType };
+// Define the RootQuery for the TaskType
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  description: 'This is the default root query provided by the application',
+  fields: {
+    task: {
+      type: TaskType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // resolve with data
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
