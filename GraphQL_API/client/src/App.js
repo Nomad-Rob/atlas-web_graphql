2e9e78deb69f30b4be266665facb05eed7c50f7c
Nomad-Rob <rfarley89@gmail.com>
2024-03-04 16:37:50 -0600

    updated App.js in client side to setup applo client

--- GraphQL_API/client/src/App.js
+++ GraphQL_API/client/src/App.js
@@ -2,27 +2,31 @@
 /*
 ...
 */
+import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
 // components
 import TaskList from './components/TaskList';
 import AddTask from './components/AddTask';
 import AddProject from './components/AddProject';
+
 // apollo client setup
-/*
-...
-*/
+const client = new ApolloClient({
+  uri: 'http://localhost:8080/graphql'
+});
+
 function App() {
-  return ( <
-    div id = "main" >
-    <
-    div id = "bg" > < /div> <
-    h1 > Holberton school tasks < /h1> <
-    TaskList / >
-    <
-    AddProject / >
-    <
-    AddTask / >
-    <
-    /div>
+  return (
+    <ApolloProvider client={ client }>
+      <div id="main">
+        <div id="bg"></div>
+        <h1>Holberton School Tasks</h1>
+        <TaskList />
+        <AddProject />
+        <AddTask />
+      </div>
+    </ApolloProvider>
   );
 }
 
