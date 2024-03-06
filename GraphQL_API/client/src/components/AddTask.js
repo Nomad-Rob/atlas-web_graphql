3b5a3731ea4cf3cd47efcc6236bb637ec34486e9
Nomad-Rob <rfarley89@gmail.com>
2024-03-06 14:21:29 -0600

    Query variables and composing queries part two . Adding mutations and fetch on addtask.js

--- GraphQL_API/client/src/components/AddTask.js
+++ GraphQL_API/client/src/components/AddTask.js
@@ -4,36 +4,98 @@ import { gql } from 'apollo-boost';
 import { graphql } from 'react-apollo';
 import { getProjectsQuery, getTasksQuery, addTaskMutation } from '../queries/queries';
 
-const getProjectsQuery = gql`
-  {
-    projects {
-      id
-      title
-    }
-  }
-`;
-
 function AddTask(props) {
+  const [inputs, setInputs] = useState({
+    title: '',
+    weight: 1,
+    description: '',
+    projectId: ''
+  });
   function displayProjects() {
-    const { loading, projects } = props.data;
-
-    if (loading) {
-      return <option>Loading projects...</option>;
+    var data = props.getProjectsQuery;
+    if (data) {
+      return <option disabled>Loading projects...</option>;
     } else {
-      return projects.map(project => (
-        <option key={project.id} value={project.id}>
-          {project.title}
-        </option>
-      ));
+      return data.projects.map(project => {
+        return (
+          <option key={project.id} value={project.id}>{project.title}</option>
+        );
+      });
     }
   }
+
+  const handleChange = (e) => {
+        const newInputs = {
+          ...inputs
+        };
+        if (e.target.name === "weight") newInputs[e.target.name] = parseInt(e.target.value)
+        else newInputs[e.target.name] = e.target.value
+        setInputs(newInputs)
+  }
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    props.addTaskMutation({
+      variables: {
+        title: inputs.title,
+        weight: inputs.weight,
+        description: inputs.description,
+        projectId: inputs.projectId
+      },
+      refetchQueries: [{ query: getTasksQuery }]
+    });
+  };
+
 
   return (
-    <select>
-      <option>Select Project</option>
-      {displayProjects()}
-    </select>
+  <form className="task" id="add-task" onSubmit={submitForm}>
+    <div className="field">
+      <label>Task title:</label>
+      <input
+        type="text"
+        name="title"
+        onChange={handleChange}
+        value={inputs.title}
+        required
+      />
+    </div>
+    <div className="field">
+      <label>Weight:</label>
+      <input
+        type="number"
+        name="weight"
+        onChange={handleChange}
+        value={inputs.weight}
+        required
+      />
+    </div>
+    <div className="field" >
+      <label>description:</label>
+      <textarea
+        name="description"
+        onChange={handleChange}
+        value={inputs.description}
+        required
+      />
+    </div>
+    <div className="field">
+      <label>Project:</label>
+      <select
+        name="projectId"
+        onChange={handleChange}
+        value={inputs.projectId}
+        required
+      >
+        <option value = "" disabled>Select project</option>
+        {displayProjects()}
+      </select>
+    </div>
+    <button>+</button>
+    </form>
   );
 }
 
-export default graphql(getProjectsQuery)(AddTask);
+export default compose(
+  graphql(getProjectsQuery, { name: 'getProjectsQuery' }),
+  graphql(addTaskMutation, { name: 'addTaskMutation' })
+  )(AddTask);
